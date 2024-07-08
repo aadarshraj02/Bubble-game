1c5c245c255dc4b4822b2b42e14b8ddc20bc3f27
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-08 17:12:51 +0530

    added timer functionality

--- script.js
+++ script.js
@@ -5,4 +5,16 @@ for (let i = 1; i <= 147; i++) {
 }
 document.querySelector("#p-bottom").innerHTML = clutter;
 
+let timer = 60;
+function runTimer() {
+  var time = setInterval(() => {
+    if (timer > 0) {
+      timer--;
+      document.querySelector("#timer-val").innerHTML = timer;
+    } else {
+      clearInterval(time);
+    }
+  }, 1000);
+}
 
+runTimer();
